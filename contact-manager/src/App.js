33bd263b70496ef29349ea0d32cace229d3e763d
SIR CALCULUS <calculus2uk@gmail.com>
2018-07-31 23:21:35 +0300

    propTypes validation

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -8,8 +8,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact
+          name="John Doe"
+          email="john.doe@gmail.com"
+          phone="333-333-333"
+        />
+        <Contact
+          name="Karen Smith"
+          email="karen.smith@gmail.com"
+          phone="555-555-5555"
+        />
       </div>
     );
   }
