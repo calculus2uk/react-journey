2132b667062863053ae4cdbfdc734bf5ad9ecb90
SIR CALCULUS <calculus2uk@gmail.com>
2018-08-17 12:09:00 +0300

    add funtionality of fetching contacts from fake json

--- contact-manager-redux/src/actions/contactAction.js
+++ contact-manager-redux/src/actions/contactAction.js
@@ -1,9 +1,12 @@
 import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT } from './types';
+import axios from 'axios';
 
-export const getContacts = () => {
-  return {
-    type: GET_CONTACTS
-  };
+export const getContacts = () => async dispatch => {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+  dispatch({
+    type: GET_CONTACTS,
+    payload: res.data
+  });
 };
 
 export const addContact = contact => {
@@ -13,9 +16,10 @@ export const addContact = contact => {
   };
 };
 
-export const deleteContact = id => {
-  return {
+export const deleteContact = id => async dispatch => {
+  await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+  dispatch({
     type: DELETE_CONTACT,
     payload: id
-  };
+  });
 };
