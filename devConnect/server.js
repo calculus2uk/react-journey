5e2e5f3587c83fbbfdca0e38b468740b6ef4e83f
SIR CALCULUS <calculus2uk@gmail.com>
2018-10-09 21:49:46 +0300

    complete the user and users section

--- devConnect/server.js
+++ devConnect/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const passportFile = require('./config/passport');
 
 //Routes
 const users = require('./routes/api/users');
+//const profile = require('./routes/api/profile');
 
 //App initialization
 const app = express();
-
-const port = process.env.PORT || 5000;
 
 mongoose
   .connect(
@@ -17,10 +19,19 @@ mongoose
   .then(() => console.log('Sucesfully connected to MongoDb...'))
   .catch(err => console.log('Could not connect to DB'));
 
-app.get('/', (req, res) => res.send('Hello World'));
+// Pasport Middleware
+app.use(passport.initialize());
+passportFile(passport);
+
+//Passport Config
 
 //Use Middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/api/users', users);
+//app.use('/api/profile', profile);
 
 //Listening on Port
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
